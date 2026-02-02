c800952bbba5d2c6ffeeaac340dd00600ecbf8ab
Azmi Deliaslan <azmideliaslan@gmail.com>
2026-02-02 23:33:46 +0300

    .
    .

--- bist_symbols.py
+++ bist_symbols.py
@@ -5,13 +5,18 @@
 # - YFinance'ta veri gelmeyenler analiz sırasında otomatik atlanır
 
 BIST_SYMBOLS = [
-    "AEFES", "AKBNK", "AKCNS", "AKENR", "AKSA", "AKSEN",
-    "ALARK", "ARCLK", "ASELS", "BIMAS", "BRISA",
-    "DOAS", "EKGYO", "ENJSA", "EREGL", "FROTO",
-    "GARAN", "GUBRF", "HALKB", "ISCTR",
-    "KCHOL", "KOZAA", "KOZAL", "KRDMD",
-    "ODAS", "OTKAR", "PETKM", "PGSUS",
-    "SAHOL", "SASA", "SISE", "TAVHL",
-    "TCELL", "THYAO", "TKFEN", "TOASO",
-    "TSKB", "TUPRS", "ULKER", "YKBNK"
+    "AEFES","AGHOL","AKBNK","AKCNS","AKSA","AKSEN",
+    "ALARK","ALBRK","ANHYT","ARCLK","ASELS","ASTOR",
+    "BIMAS","BRISA","CIMSA","DOAS","ECILC","EKGYO",
+    "ENJSA","ENKAI","EREGL","EUPWR","FROTO","GARAN",
+    "GENIL","GESAN","GUBRF","HALKB","HEKTS","ISCTR",
+    "ISDMR","KARSN","KCHOL","KLSER","KOZAA","KOZAL",
+    "MAVI","ODAS","OTKAR","OYAKC","PETKM","PGSUS",
+    "QUAGR","SAHOL","SASA","SELEC","SISE","SMRTG",
+    "SOKM","TAVHL","TCELL","THYAO","TKFEN","TOASO",
+    "TTKOM","TTRAK","TUKAS","TUPRS","VESBE","VESTL",
+    "YATAS","YKBNK","YYLGD","ZOREN",
+    "AKFGY","AKSGY","ALCTL","BRSAN","CCOLA","CANTE",
+    "GWIND","IPEKE","KONTR","LOGO","NTHOL","ODMD",
+    "SNGYO","TSKB","ULKER","VAKBN","YUNSA","ARASE"
 ]
