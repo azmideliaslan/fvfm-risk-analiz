c6a5f59e20bf80d4e30efe54e09b7cfe0e17b718
Azmi Deliaslan <azmideliaslan@gmail.com>
2026-02-08 00:30:50 +0300

    api added

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:8000";
+const API = "https://fvfm-risk-analiz.onrender.com";
 
 const select = document.getElementById("stockSelect");
 const resultDiv = document.getElementById("result");
